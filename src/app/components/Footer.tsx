2e8c56d86cf6934471b8cc9619f1f6d0633ccf09
Vikram <github@vknc.mozmail.com>
2025-02-27 23:28:32 -0500

    Add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-900 text-white p-4 text-center mt-4">
+            <p>
+                &copy; {new Date().getFullYear()} | Site developed for W4VKU by{" "}
+                <a
+                    href="https://www.vikramk.dev"
+                    className="text-blue-400 hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Vikram Krishnakumar
+                </a>
+
+            </p>
+        </footer>
+    );
+}
